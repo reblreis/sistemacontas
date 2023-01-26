05abe45083ea3941439610ed055d2110bbf1b5d1
reblreis <rebreis88@gmail.com>
2023-01-26 20:23:31 -0300

    Entidades e script do banco de dados

---
+++ src/main/java/br/com/cotiinformatica/entities/Conta.java
@@ -0,0 +1,27 @@
+package br.com.cotiinformatica.entities;
+
+import java.util.Date;
+
+import br.com.cotiinformatica.enums.TipoConta;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Conta {
+
+	private Integer idConta;
+	private String nomeString;
+	private Double valor;
+	private Date data;
+	private TipoConta tipo;
+	private String observacoes;
+	private Usuario usuario;
+
+}
---
+++ src/main/java/br/com/cotiinformatica/enums/TipoConta.java
@@ -0,0 +1,6 @@
+package br.com.cotiinformatica.enums;
+
+public enum TipoConta {
+	PAGAR, 
+	RECEBER
+}
